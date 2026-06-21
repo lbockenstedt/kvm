93b30d396b4b3306a62e2983f101315b15292572
Lance Bockenstedt <lrb@hpe.com>
2026-06-21 06:22:49 -0500

    chore: standardize logging format (timestamp + source)

--- src/control_plane.py
+++ src/control_plane.py
@@ -20,7 +20,8 @@ except ImportError:
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger("KVMControlPlane")
 
